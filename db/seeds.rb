e73e8e54e57b72233c30f5e043d2900caead51e8
Kelvin Lee <kelvin_lee@leaderradio.cn>
2019-04-09 13:59:56 +0800

    starting on seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,29 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# USERS
+# name
+# photo
+# wechat_id
+# description
+# latitude
+# longitude
+
+# BOOKINGS
+# user_id
+# event_id
+# attendance
+# review
+
+# EVENTS
+# organizer_id
+# type
+# capacity
+# description
+# start_time
+# end_time
+# location
+# latitude
+# longitude
+# photo
+
+make 300 USERS
+make 50 EVENTS in Shanghai
+make 5-20 bookings per event
