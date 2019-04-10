cc864f48d8958f735d67a4fa725b2bd2018ad3a1
Kelvin Lee <kelvin_lee@leaderradio.cn>
2019-04-10 11:32:44 +0800

    correcting unsaved file event.rb

---
+++ app/models/event.rb
@@ -0,0 +1,9 @@
+class Event < ApplicationRecord
+  belongs_to :user
+  has_many :bookings
+  validates :user_id, presence: true
+  validates :activity_type, presence: true
+  validates :start_time, presence: true
+  validates :end_time, presence: true
+  validates :location, presence: true
+end
